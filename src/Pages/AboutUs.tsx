18ee8210ce1d92f5d01a8883bcc35008451e935d
kate <katepgoesintothesunset@gmail.ru>
2023-03-15 15:57:09 +0300

    feat: add pages and routes

---
+++ src/Pages/AboutUs.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function AboutUs() {
+  return <h1>Hello! Sorry, there is nothing special here yet, but it may appear later</h1>;
+}
---
+++ src/Pages/PostsPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function PostsPage() {
+  return (
+    <div>
+      <h1>Hello! Sorry, there is nothing here yet, but it will definitely appear later</h1>
+    </div>
+  );
+}
